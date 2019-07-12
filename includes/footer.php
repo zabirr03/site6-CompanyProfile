1013c6c6f5e576a6eed638c269b09906972d584c
WajeehaParker <wajeeha.parker@gmail.com>
2019-07-12 19:25:42 +0500

    uploading

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<div id="footer" class="row">
+    <div class="col-md-6" id="footer-left">
+        <span id="copyright">&copy; Wajeeha Parker, 2019</span>
+    </div>
+    <div class="col-md-6" id="footer-right">
+       <span id="tc"> <a href="#">Terms and Conditions</a> | <a href="#">Privacy Policy</a></span>
+       </div>
+</div>
